35af916b3dcff86f63c37ccbed9aa66285338381
Wei Jun <tiahweijun1999@gmail.com>
2021-06-15 14:12:04 +0800

    fix

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { createUploadLink } from "apollo-upload-client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createUploadLink({
-  uri: `http://foodguru-wj.herokuapp.com/graphql`,
+  uri: `https://foodguru-wj.herokuapp.com/graphql`,
 }); //foodguru-wj.herokuapp.com
 
 const authLink = setContext((_, { headers }) => {
